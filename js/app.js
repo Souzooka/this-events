ec4cf8ae2c5bb50ed3f0ca16e4640e64b9e038a6
Souzooka <bazookaharwell@gmail.com>
2017-01-25 21:35:45 -1000

    complete

--- js/app.js
+++ js/app.js
@@ -1,4 +1,23 @@
 //Secret Menu Challenge
 /*Add event listeners to each restaurant name that will show and hide (toggle) the secret menu item for that particular restaurant.*/
 
+var restaurantName = document.querySelectorAll(".name");
+
+for (var i = 0; i < restaurantName.length; i++) {
+	restaurantName[i].addEventListener("click", showMenu);
+	restaurantName[i].querySelectorAll(".menu")[0].visible = false;
+}
+
+function showMenu() {
+	var restaurantMenu = this.querySelectorAll(".menu")[0];
+		if (restaurantMenu.visible === false) {
+			restaurantMenu.style.display = "initial";
+			restaurantMenu.visible = false;
+		}
+		else {
+			restaurantMenu.style.display = "none";
+			restaurantMenu.visible = true;
+		}
+}
+
 
